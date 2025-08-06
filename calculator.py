d776adb751b4b24e9bfef53d275744deff566001
COSMO-911 <cosmomit21@gmail.com>
2025-08-06 20:55:53 +0530

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-print("welcome to calculator with 0 errors :P ")
+import sys
 while True:
     operator = input("Enter operator: ")
     result = 0 
@@ -18,19 +18,28 @@ while True:
             result= num1*num2
         elif operator=="/":
             divide_again = "y"
+            trials = 0
             while divide_again == "y":
+                
+                if trials >=3:
+                    break
                 try:
                     result= num1 /num2
                     break
                 except ZeroDivisionError:
                     print("cant divide by zero")
+                    trials+=1
                     divide_again = input("press 'y' to divide again: ").lower()
                     num1 = int(input("enter number 1 : "))
                     num2 = int(input("enter number 2 : "))
+                print("too many 0 division error")
+                sys.exit()
         print(f"{num1} {operator} {num2} = {result}")
         again = input("press 'y' to reuse the calc. : ").lower()
         if again != "y":
             break
     else:
         print("choose from [+ - * /] ")
+
 print("Bye bye")
+
